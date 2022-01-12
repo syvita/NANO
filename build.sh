5cf2b177e7be042d736085ce422cf21256344fff
Linden <70739041+linden@users.noreply.github.com>
2022-01-12 01:00:27 -0800

    build: remove stale `dist-*`

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+rm -rf dist-*
+
 id=$(docker run -td --rm ubuntu)
 
 docker cp . $id:i2p-zero
 
 docker exec -ti $id bash -c  '\
   rm -rf import \
-  && rm -rf dist-* \
   && apt-get update \
   && apt-get -y install git wget zip unzip \
   && cd i2p-zero \
